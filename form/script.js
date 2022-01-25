31fe77ab16b62214d529eaffcbeeaaa5ea83ef6b
Abhishek Kalia <abhishekkalia1103@gmail.com>
2022-01-25 13:36:56 +0530

    Update script.js

--- form/script.js
+++ form/script.js
@@ -1,10 +1,9 @@
 var selectedRow = null
-
 function onFormSubmit() {
     if (validate()) {
         var formData = readFormData();
         if (selectedRow == null)
-            insertNewRecord(formData);
+            showData();
         else
             updateRecord(formData);
         resetForm();
@@ -14,27 +13,87 @@ function onFormSubmit() {
 function readFormData() {
     var formData = {};
     formData["firstName"] = document.getElementById("firstName").value;
+    formData["secondName"] = document.getElementById("secondName").value;
     formData["email"] = document.getElementById("email").value;
     formData["number"] = document.getElementById("number").value;
-    formData["secondName"] = document.getElementById("secondName").value;
+    formData["psw"] = document.getElementById("psw").value;
+
+    saveData(formData);
+
     return formData;
+}
+
+
+
+function saveData(data)
+{
+// let firstname,email,psw, secondname, number ;
+// name=document.getElementById("firstname").value;
+// secondname=document.getElementById("secondname").value;
+// email=document.getElementById("email").value;
+
+// psw=document.getElementById("psw").value;
+
+// number =document.getElementById("number").value;
+
+let user_records=new Array();
+user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+if(user_records.some((v)=>{return v.email==email}))
+{
+  alert("duplicate data");
+}
+else
+{
+  user_records.push({
+  "firstname":data.firstName,
+  "secondname" : data.secondName,
+  "email":data.email,
+  "psw":data.psw,
+  "number" : data.number,
+})
+localStorage.setItem("users",JSON.stringify(user_records));
+}
+
 }
 
 function insertNewRecord(data) {
     var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
+    var newRow = table.insertRow(5);
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.firstName;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.email;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.number;
-    cell4 = newRow.insertCell(3);
+    cell4 = newRow.insertCell(1);
     cell4.innerHTML = data.secondName;
+    cell2 = newRow.insertCell(2);
+    cell2.innerHTML = data.email;
+    cell3 = newRow.insertCell(3);
+    cell3.innerHTML = data.number;
+   
     cell4 = newRow.insertCell(4);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
+
+
 }
+
+function showData()
+{
+  document.getElementById("employeeList").innerHTML="";
+  let user_records=new Array();
+user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+  if(user_records)
+  {
+    for(let i=0;i<user_records.length;i++)
+    {
+        
+  //     let addDiv=document.createElement('div');
+  // addDiv.className="row";
+  // addDiv.innerHTML='  <div class="col-sm-4" style="padding: 10px;">'+user_records[i].firstName+'</div><div class="col-sm-4" style="padding: 10px;">'+user_records[i].email+'</div><div class="col-sm-4" style="padding: 10px;">'+user_records[i].psw+'</div>';
+  // document.getElementById("employeeList").appendChild(addDiv);
+        insertNewRecord(user_records[i]);
+
+    }
+  }
+  }
 
 function resetForm() {
     document.getElementById("firstName").value = "";
@@ -47,21 +106,38 @@ function resetForm() {
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
     document.getElementById("firstName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("number").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("secondName").value = selectedRow.cells[3].innerHTML;
+     document.getElementById("secondName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("number").value = selectedRow.cells[3].innerHTML;
+    // document.getElementById("psw").value = selectedRow.cells[3].innerHTML;
+
+    var user_records=new Array();
+     user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+   
+        user_records[selectedRow.rowIndex].firstName = document.getElementById("firstName").value;
+        user_records[selectedRow.rowIndex].secondName = document.getElementById("secondName").value;
+        user_records[selectedRow.rowIndex].email = document.getElementById("email").value;
+        user_records[selectedRow.rowIndex].number = document.getElementById("number").value;
+        localStorage.setItem('users', JSON.stringify(user_records));
 }
 function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.firstName;
-    selectedRow.cells[1].innerHTML = formData.email;
-    selectedRow.cells[2].innerHTML = formData.number;
-    selectedRow.cells[3].innerHTML = formData.secondName;
+    selectedRow.cells[1].innerHTML = formData.secondName;
+    selectedRow.cells[2].innerHTML = formData.email;
+    selectedRow.cells[3].innerHTML = formData.number;
+    
 }
 
 function onDelete(td) {
+
     if (confirm('Are you sure to delete this record ?')) {
         row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
+       // let index = faveGif.findIndex(id => id == "ewfewfew");
+       var user_records=new Array();
+       user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+       user_records.splice(row.rowIndex, 1);
+       localStorage.setItem('users',JSON.stringify(user_records));
+        document.getElementById("users").deleteRow(row.rowIndex);
         resetForm();
     }
 }
@@ -99,6 +175,27 @@ function validate() {
         if (!document.getElementById("firstNameValidationError").classList.contains("hide"))
             document.getElementById("firstNameValidationError").classList.add("hide");
     }
+    if (secondName == "") {
+        isValid = false;
+        document.getElementById('secondNameValidationError').innerHTML = "Required";
+        document.getElementById("secondNameValidationError").classList.remove("hide");
+    } 
+    else if((secondName.length <= 2) || (secondName.length > 20)) {
+        isValid = false;
+        document.getElementById('secondNameValidationError').innerHTML =" ** Second name length must be between 3 and 20";
+        document.getElementById("secondNameValidationError").classList.remove("hide");  
+    }
+    else  if(!isNaN(secondName)){
+        document.getElementById('secondNameValidationError').innerHTML =" ** only characters are allowed";
+         document.getElementById("secondNameValidationError").classList.remove("hide");  
+        isValid = false; 
+    }
+
+    else {
+        isValid = true;
+        if (!document.getElementById("secondNameValidationError").classList.contains("hide"))
+            document.getElementById("secondNameValidationError").classList.add("hide");
+    }
 
      if(email == ""){
                isValid = false;
@@ -111,7 +208,7 @@ function validate() {
                 document.getElementById("emailValidationError").classList.remove("hide");
             }
 
-            else if((email.charAt(email.length-4)!='.') && (emails.charAt(emails.length-3)!='.')){
+            else if((email.charAt(email.length-4)!='.') && (emails.charAt(email.length-3)!='.')){
                 isValid = false;
                 document.getElementById('email').innerHTML =" ** . Invalid Position";
                 document.getElementById("emailValidationError").classList.remove("hide");
@@ -143,27 +240,7 @@ function validate() {
                     document.getElementById("numberValidationError").classList.add("hide");
             }
 
-    if (secondName == "") {
-        isValid = false;
-        document.getElementById('secondNameValidationError').innerHTML = "Required";
-        document.getElementById("secondNameValidationError").classList.remove("hide");
-    } 
-    else if((secondName.length <= 2) || (secondName.length > 20)) {
-        isValid = false;
-        document.getElementById('secondNameValidationError').innerHTML =" ** Second name length must be between 3 and 20";
-        document.getElementById("secondNameValidationError").classList.remove("hide");  
-    }
-    else  if(!isNaN(secondName)){
-        document.getElementById('secondNameValidationError').innerHTML =" ** only characters are allowed";
-         document.getElementById("secondNameValidationError").classList.remove("hide");  
-        isValid = false; 
-    }
-
-    else {
-        isValid = true;
-        if (!document.getElementById("secondNameValidationError").classList.contains("hide"))
-            document.getElementById("secondNameValidationError").classList.add("hide");
-    }
+    
 
     if(psw == ""){
          isValid = false;
